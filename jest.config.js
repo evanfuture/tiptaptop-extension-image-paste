f5e3d9b4d93297b85c1dd473f022b31e68b760f1
evanfuture <evan@evanpayne.com>
2021-10-11 19:16:29 +0200

    first commit

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'tiptaptop-extension-image-paste',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/tiptaptop/extension-image-paste',
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { ImagePaste } from './lib/image-paste';
+
+export * from './lib/image-paste';
+
+export default ImagePaste;
